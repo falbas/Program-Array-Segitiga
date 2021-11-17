3186e8f816cdef45559835531740112a4566f9d3
falbas <farhanalbas30@gmail.com>
2021-11-17 14:15:50 +0700

    Add Triangular Array Type 4

--- ArraySegitiga.py
+++ ArraySegitiga.py
@@ -1,21 +1,34 @@
 def cetakarr(arr, n, m):
-    gap = 0
+    gap = 0  # variabel untuk memberi spasi antar elemen
     for i in range(n):
         for j in range(m):
-            gap = max(gap, len(arr[i][j]))
+            gap = max(gap, len(arr[i][j])) # mencari nilai maksimal spasi
     gap += 1
 
     for i in range(n):
         for j in range(m):
-            print(arr[i][j], end="")
+            print(arr[i][j], end="") # mencetak elemen array
             for k in range(gap - len(arr[i][j])):
-                print(" ", end='')
+                print(" ", end='') # mencetak spasi sesuai besar variabel gap
         print()
 
 
 def segitiga1():
-    print("\n-----ARRAY SEGITIGA TIPE 1-----\n")
-    n = int(input(f'Masukan Ukuran Array (Integer): '))
+    print('''
+-----ARRAY SEGITIGA TYPE 1-----
+Hasil dari Type 1 adalah:
+- Array A Upper Triangular berorder N * N
+- Array B Lower Triangular berorder (N-1) * (N-1)
+- A dan B disimpan di Array C berorder 3 * 3
+    ''')
+
+    n = 0
+    while n < 3 or n > 5:
+        n = int(input(f'Masukkan ukuran array (3-5): '))
+        if n >= 3 and n <= 5:
+            break
+        print("Ukuran array harus antara 3-5")
+
     a = [['0' for i in range(n)] for i in range(n)]
     b = [['0' for i in range(n - 1)] for i in range(n - 1)]
     c = [['0' for i in range(n)] for i in range(n)]
@@ -27,7 +40,7 @@ def segitiga1():
                 a[i][j] = input(f'- Masukkan Elemen A[{i+1},{j+1}]: ')
                 c[i][j] = a[i][j]
 
-    print(f"\nArray A (Upper Triangular) dengan order {n}*{n}:")
+    print(f"\nArray A (Upper Triangular) dengan order {n} * {n}:")
     cetakarr(a, n, n)
     print()
 
@@ -37,26 +50,39 @@ def segitiga1():
                 b[i][j] = input((f'- Masukkan Elemen B[{i+1},{j+1}]: '))
                 c[i + 1][j] = b[i][j]
 
-    print(f"\nArray B (Lower Triangular) dengan order {n-1}*{n-1}:")
+    print(f"\nArray B (Lower Triangular) dengan order {n-1} * {n-1}:")
     cetakarr(b, n - 1, n - 1)
 
     input("\nTekan Enter Untuk Melihat Hasil!\n")
     print("----------- H A S I L -----------")
 
-    print("\nJadi array segitiga tipe 1 dari")
-    print(f"array A (Upper Triangular) dengan order {n}*{n}:")
+    print("\nJadi Array Segitiga Type 1 dari")
+    print(f"Array A (Upper Triangular) dengan order {n} * {n}:")
     cetakarr(a, n, n)
 
-    print(f"\ndan array B (Lower Triangular) dengan order {n-1}*{n-1}:")
+    print(f"\ndan Array B (Lower Triangular) dengan order {n-1} * {n-1}:")
     cetakarr(b, n - 1, n - 1)
 
-    print(f"\nHasil akhirnya adalah array dengan order {n}*{n}:")
+    print(f"\nHasil akhirnya adalah Array C dengan order {n} * {n}:")
     cetakarr(c, n, n)
 
 
 def segitiga2():
-    print("\n-----ARRAY SEGITIGA TIPE 2-----\n")
-    n = int(input(f'Masukan Ukuran Array (Integer): '))
+    print('''
+-----ARRAY SEGITIGA TYPE 2-----
+Hasil dari Type 2 adalah:
+- Array A Upper Triangular berorder N * N
+- Array B Lower Triangular berorder N * N
+- A dan B disimpan di Array C berorder N * (N+1)
+    ''')
+
+    n = 0
+    while n < 3 or n > 5:
+        n = int(input(f'Masukkan ukuran array (3-5): '))
+        if n >= 3 and n <= 5:
+            break
+        print("Ukuran array harus antara 3-5")
+
     a = [['0' for i in range(n)] for i in range(n)]
     b = [['0' for i in range(n)] for i in range(n)]
     c = [['0' for i in range(n + 1)] for i in range(n)]
@@ -66,12 +92,9 @@ def segitiga2():
         for j in range(n):
             if j >= i:
                 a[i][j] = input(f'- Masukkan Elemen A[{i+1},{j+1}]: ')
+                c[i][j + 1] = a[i][j]
 
-    for i in range(n):
-        for j in range(n):
-            c[i][j + 1] = a[i][j]
-
-    print(f"\nArray A (Upper Triangular) dengan order {n}*{n}:")
+    print(f"\nArray A (Upper Triangular) dengan order {n} * {n}:")
     cetakarr(a, n, n)
     print()
 
@@ -79,32 +102,42 @@ def segitiga2():
         for j in range(n):
             if j <= i:
                 b[i][j] = input((f'- Masukkan Elemen B[{i+1},{j+1}]: '))
-
-    for i in range(n):
-        for j in range(n):
-            if j <= i:
                 c[i][j] = b[i][j]
 
-    print(f"\nArray B (Lower Triangular) dengan order {n}*{n}:")
+    print(f"\nArray B (Lower Triangular) dengan order {n} * {n}:")
     cetakarr(b, n, n)
 
     input("\nTekan Enter Untuk Melihat Hasil!\n")
     print("----------- H A S I L -----------")
 
-    print("\nJadi array segitiga tipe 2 dari")
-    print(f"array A (Upper Triangular) dengan order {n}*{n}:")
+    print("\nJadi Array Segitiga Type 2 dari")
+    print(f"Array A (Upper Triangular) dengan order {n} * {n}:")
     cetakarr(a, n, n)
 
-    print(f"\ndan array B (Lower Triangular) dengan order {n}*{n}:")
+    print(f"\ndan Array B (Lower Triangular) dengan order {n} * {n}:")
     cetakarr(b, n, n)
 
-    print(f"\nHasil akhirnya adalah array dengan order {n}*{n+1}:")
+    print(f"\nHasil akhirnya adalah Array C dengan order {n} * {n+1}:")
     cetakarr(c, n, n + 1)
 
 
 def segitiga3():
-    print("\n-----ARRAY SEGITIGA TIPE 3-----\n")
-    n = int(input(f'Masukan Ukuran Array (Integer): '))
+    print('''
+-----ARRAY SEGITIGA TYPE 3-----
+Hasil dari Type 3 adalah:
+- Array A Upper Triangular berorder N * N
+- Array B Upper Triangular berorder N * N
+- Transpose Array B menjadi Lower Triangular berorder N * N
+- A dan B disimpan di Array C berorder N * (N+1)
+    ''')
+
+    n = 0
+    while n < 3 or n > 5:
+        n = int(input(f'Masukkan ukuran array (3-5): '))
+        if n >= 3 and n <= 5:
+            break
+        print("Ukuran array harus antara 3-5")
+
     a = [['0' for i in range(n)] for i in range(n)]
     b = [['0' for i in range(n)] for i in range(n)]
     c = [['0' for i in range(n + 1)] for i in range(n)]
@@ -114,12 +147,9 @@ def segitiga3():
         for j in range(n):
             if j >= i:
                 a[i][j] = input(f'- Masukkan Elemen A[{i+1},{j+1}]: ')
+                c[i][j + 1] = a[i][j]
 
-    for i in range(n):
-        for j in range(n):
-            c[i][j + 1] = a[i][j]
-
-    print(f"\nArray A (Upper Triangular) dengan order {n}*{n}:")
+    print(f"\nArray A (Upper Triangular) dengan order {n} * {n}:")
     cetakarr(a, n, n)
     print()
 
@@ -128,47 +158,104 @@ def segitiga3():
             if j >= i:
                 b[i][j] = input((f'- Masukkan Elemen B[{i+1},{j+1}]: '))
 
-    print(f"\nArray B (Upper Triangular) dengan order {n}*{n}:")
+    print(f"\nArray B (Upper Triangular) dengan order {n} * {n}:")
     cetakarr(b, n, n)
 
     for i in range(n):
         for j in range(n):
             if j >= i:
                 b[i][j], b[j][i] = b[j][i], b[i][j]
-
-    print(f"\nTranspose Array B (Lower Triangular) dengan order {n}*{n}:")
-    cetakarr(b, n, n)
-
-    for i in range(n):
-        for j in range(n):
             if j <= i:
                 c[i][j] = b[i][j]
+
+    print(f"\nTranspose Array B (Lower Triangular) dengan order {n} * {n}:")
+    cetakarr(b, n, n)
 
     input("\nTekan Enter Untuk Melihat Hasil!\n")
     print("----------- H A S I L -----------")
 
-    print("\nJadi array segitiga tipe 3 dari")
-    print(f"array A (Upper Triangular) dengan order {n}*{n}:")
+    print("\nJadi Array Segitiga Type 3 dari")
+    print(f"array A (Upper Triangular) dengan order {n} * {n}:")
     cetakarr(a, n, n)
 
-    print(f"\ndan transpose array B (Lower Triangular) dengan order {n}*{n}:")
+    print(f"\ndan Transpose Array B (Lower Triangular) dengan order {n}*{n}:")
     cetakarr(b, n, n)
 
-    print(f"\nHasil akhirnya adalah array dengan order {n}*{n+1}:")
+    print(f"\nHasil akhirnya adalah Array C dengan order {n} * {n+1}:")
     cetakarr(c, n, n + 1)
 
 
 def segitiga4():
-    print("Array Segitiga 4 Coming Soon")
+    print('''
+-----ARRAY SEGITIGA TYPE 4-----
+Hasil dari Type 4 adalah:
+- Array A Upper Triangular berorder N * N
+- Array B Upper Triangular berorder N * N
+- Transpose Array A menjadi Lower Triangular berorder N * N
+- A dan B disimpan di Array C berorder N * (N+1)
+    ''')
+
+    n = 0
+    while n < 3 or n > 5:
+        n = int(input(f'Masukkan ukuran array (3-5): '))
+        if n >= 3 and n <= 5:
+            break
+        print("Ukuran array harus antara 3-5")
+
+    a = [['0' for i in range(n)] for i in range(n)]
+    b = [['0' for i in range(n)] for i in range(n)]
+    c = [['0' for i in range(n + 1)] for i in range(n)]
+    print()
+
+    for i in range(n):
+        for j in range(n):
+            if j >= i:
+                a[i][j] = input(f'- Masukkan Elemen A[{i+1},{j+1}]: ')
+
+    print(f"\nArray A (Upper Triangular) dengan order {n} * {n}:")
+    cetakarr(a, n, n)
+
+    for i in range(n):
+        for j in range(n):
+            if j >= i:
+                a[i][j], a[j][i] = a[j][i], a[i][j]
+            if j <= i:
+                c[i][j] = a[i][j]
+
+    print(f"\nTranspose Array A (Lower Triangular) dengan order {n} * {n}:")
+    cetakarr(a, n, n)
+    print()
+
+    for i in range(n):
+        for j in range(n):
+            if j >= i:
+                b[i][j] = input(f'- Masukkan Elemen B[{i+1},{j+1}]: ')
+                c[i][j + 1] = b[i][j]
+
+    print(f"\nArray B (Upper Triangular) dengan order {n} * {n}:")
+    cetakarr(b, n, n)
+
+    input("\nTekan Enter Untuk Melihat Hasil!\n")
+    print("----------- H A S I L -----------")
+
+    print("\nJadi array segitiga tipe 4 dari")
+    print(f"Transpose array A (Upper Triangular) dengan order {n} * {n}:")
+    cetakarr(a, n, n)
+
+    print(f"\ndan array B (Lower Triangular) dengan order {n} * {n}:")
+    cetakarr(b, n, n)
+
+    print(f"\nHasil akhirnya adalah array C dengan order {n} * {n+1}:")
+    cetakarr(c, n, n + 1)
 
 
 def menu():
     print("\n-----PROGRAM ARRAY SEGITIGA-----")
-    print("1. Array Segitiga Tipe 1")
-    print("2. Array Segitiga Tipe 2")
-    print("3. Array Segitiga Tipe 3")
-    print("4. Array Segitiga Tipe 4")
-    pilihan = input("Pilih jenis array segitiga: ")
+    print("1. Array Segitiga Type 1 order N * N")
+    print("2. Array Segitiga Type 2 order N * (N+1)")
+    print("3. Array Segitiga Type 3 order N * (N+1)")
+    print("4. Array Segitiga Type 4 order N * (N+1)")
+    pilihan = input("Pilih jenis Array Segitiga: ")
 
     if pilihan == "1":
         segitiga1()
@@ -179,7 +266,7 @@ def menu():
     elif pilihan == "4":
         segitiga4()
     else:
-        print("Tidak ada dalam pilihan")
+        print("Nomor tersebut tidak ada dalam pilihan")
 
 
 stop = False
